741ff3094d06d2a73aa98ecad52a85c48a19c60f
VolgaKurvar <kurvan1112@gmail.com>
2020-07-13 23:22:04 +0900

    Revert "ロックが正常に出来ていないのを修正"
    This reverts commit 4c2db0de47ccb560f8e60b4fe1043da87131f395.

--- www/server.php
+++ www/server.php
@@ -3,19 +3,11 @@ set_time_limit(60*5);
 $data_source_file = 'data.json';
 
 if (isset($_GET['write'])) {
-    $f=fopen($data_source_file, 'r');
-    flock($f, LOCK_EX);
-    $json='';
-    while ($get = fgets($f, 1024)) {
-        $json .= $get;
-    }
-    $json = mb_convert_encoding($json, 'UTF8', 'ASCII,JIS,UTF-8,EUC-JP,SJIS-WIN');
-    $json =json_decode($json, true);
+    $json = loadJson($data_source_file);
     $data=json_decode($_GET['write'], true);
     $data['timestamp']=time();
     array_push($json, $data);
     file_put_contents($data_source_file, str_replace(',', ",\n", json_encode($json)));
-    flock(fopen($data_source_file, 'r'), LOCK_UN);
     exit;
 }
 while (true) {
@@ -23,9 +15,7 @@ while (true) {
     clearstatcache();
     $last_change_in_data_file = filemtime($data_source_file);
     if ($last_ajax_call == null || $last_change_in_data_file > $last_ajax_call) {
-        flock(fopen($data_source_file, 'r'), LOCK_SH);
         $json=loadJson($data_source_file);
-        flock(fopen($data_source_file, 'r'), LOCK_UN);
         $data=[];
         foreach ($json as $line) {
             if (isset($_GET['timestamp'])&&$line['timestamp']<=$_GET['timestamp']) { //更新要求秒以前のデータは飛ばす
